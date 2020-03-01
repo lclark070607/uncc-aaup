bc49dec3a2fcacaa08f8e3a37f7c5e893bf3bcc9
Louise Clark <louiseclark@Louises-MacBook-Pro.local>
2020-03-01 00:15:45 -0500

    adding aria to navigation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const navSlide = () => {
     let menu = document.querySelector('.hamburger-menu');
-    let nav = document.querySelector('.nav-links');
+    let nav = document.querySelector('.nav');
     let navLinks = document.querySelectorAll('.nav li');
+    let navLinksA = document.querySelectorAll('.nav-link');
     let elementsArray = [menu, nav];
 
     elementsArray.forEach(function(elem) {
@@ -12,12 +13,15 @@ const navSlide = () => {
 
     function links() {
         nav.classList.toggle('nav-active');
+        const expanded = nav.getAttribute('aria-expanded') === 'true';
+        nav.setAttribute('aria-expanded', !expanded);
+       
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
             link.style.animation = '';
             } else {
             link.style.animation = `navLinkFade 0.3s ease forwards ${index / 5 + .1}s`;
-            } 
+            }
         });
         menu.classList.toggle('toggle');
     }
@@ -25,9 +29,23 @@ const navSlide = () => {
 navSlide();
 
 const hamburger = document.querySelector('.hamburger-menu');
+
 hamburger.addEventListener('click', setHamburgerAria);
 function setHamburgerAria() {
     const expanded = hamburger.getAttribute('aria-expanded') === 'true';
     hamburger.setAttribute('aria-expanded', !expanded);
+    let nextState = hamburger.getAttribute('aria-label') === 'menu-expanded' ? 'menu-collapsed' : 'menu-expanded';
+    hamburger.setAttribute('aria-label', nextState);
   }
+
+  hamburger.addEventListener("keyup", function(e) {
+    let code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+      let nextState = hamburger.getAttribute('aria-label') == 'menu-collapsed' ? 'menu-expanded' : 'menu-collapsed';
+      hamburger.setAttribute('aria-label', nextState);
+    }
+  });
+
+
+  
 
