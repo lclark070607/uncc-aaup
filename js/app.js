d315ad9a3a1051727ce2f7e1ac4b2910b28ec72d
Louise Clark <louiseclark@Louises-MacBook-Pro.local>
2020-04-11 11:02:52 -0400

    adding faq and calendar update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
-const navSlide = () => {
-    let menu = document.querySelector('.hamburger-menu');
-    let nav = document.querySelector('.nav');
-    let navLinks = document.querySelectorAll('.nav li');
-    let navLinksA = document.querySelectorAll('.nav-link');
+let navLinksA = document.querySelectorAll('.nav-link');
+let nav = document.querySelector('.nav');
+let menu = document.querySelector('.hamburger-menu');
+let navLinks = document.querySelectorAll('.nav li');
+
+
+// window.onload = () => {
+//     if (window.innerWidth <= 768) {
+//         nav.style.visibility = 'hidden';
+//         navLinksA.forEach((link, index) => {
+//             link.style.visibility = 'hidden';
+//         })
+//     }
+// }
+
+// // if(menu..getAttribute('aria-expanded')=='false')) {
+// //     if(document.getElementById('mytooltipbutton2').getAttribute('aria-expanded')=='true'){
+// //         document.getElementById('mytooltip2').style.visibility = 'hidden'; 
+// //         document.getElementById('mytooltipbutton2').setAttribute('aria-expanded', 'false');
+// //       } else { 
+// //           document.getElementById('mytooltip2').style.visibility = 'visible'; 
+// //           document.getElementById('mytooltipbutton2').setAttribute('aria-expanded', 'true');
+// //       };
+// // }
+
+// responsive nav slider
+const navSlide = () => {  
     let elementsArray = [menu, nav];
 
     elementsArray.forEach(function(elem) {
@@ -15,6 +37,7 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
         const expanded = nav.getAttribute('aria-expanded') === 'true';
         nav.setAttribute('aria-expanded', !expanded);
+        let visibility = nav.style.visibility === 'visible' ? 'hidden' : 'visibile';
        
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -28,23 +51,62 @@ const navSlide = () => {
 }
 navSlide();
 
+// hamburger btn a11y
 const hamburger = document.querySelector('.hamburger-menu');
 
 hamburger.addEventListener('click', setHamburgerAria);
 function setHamburgerAria() {
     const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+   
     hamburger.setAttribute('aria-expanded', !expanded);
     let nextState = hamburger.getAttribute('aria-label') === 'menu-expanded' ? 'menu-collapsed' : 'menu-expanded';
     hamburger.setAttribute('aria-label', nextState);
+
+    navLinksA.forEach((link, index) => {
+      let toggleTabindex = link.removeAttribute('hidden');
+    })
   }
 
   hamburger.addEventListener("keyup", function(e) {
     let code = (e.keyCode ? e.keyCode : e.which);
     if (code == 13) {
-      let nextState = hamburger.getAttribute('aria-label') == 'menu-collapsed' ? 'menu-expanded' : 'menu-collapsed';
+      let nextState = hamburger.getAttribute('aria-label') == 'menu-expanded' ? 'menu-collapsed' : 'menu-expanded';
       hamburger.setAttribute('aria-label', nextState);
     }
   });
+
+  // toggle polyfill
+  if (!Element.prototype.toggleAttribute) {
+    Element.prototype.toggleAttribute = function(name, force) {
+      if(force !== void 0) force = !!force 
+      
+      if (this.hasAttribute(name)) {
+        if (force) return true;
+  
+        this.removeAttribute(name);
+        return false;
+      }
+      if (force === false) return false;
+        
+      this.setAttribute(name, "");
+      return true;
+    };
+  }
+
+  //accordion
+  const accordionComponent = document.querySelector('.accordion');
+accordionComponent.addEventListener('click', (event) => {
+  accordion(event);
+});
+
+function accordion() {
+  const btn = event.target.closest('[accordion-trigger]');
+  const expanded = btn.getAttribute('aria-expanded') === 'true';
+  const target = btn.parentNode.nextElementSibling;
+
+  btn.setAttribute('aria-expanded', !expanded);
+  target.hidden = expanded;
+}
 
 
   
